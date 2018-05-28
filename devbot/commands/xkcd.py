69e76d45f25d5e82bfa3536e061804f3201ca533
Roman Peters <romanpeters1993@gmail.com>
2018-05-28 21:09:09 +0200

    Codacy compliant

--- devbot/commands/xkcd.py
+++ devbot/commands/xkcd.py
@@ -1,5 +1,5 @@
 """ Latest XKCD command. """
-import json 
+import json
 import urllib.request
 
 from devbot.registry import Command
@@ -7,9 +7,9 @@ from devbot.registry import Command
 
 @Command(["xkcd"])
 async def xkcd():
-    url = 'https://xkcd.com/info.0.json'
-    response = urllib.request.urlopen(url)
+    response = urllib.request.urlopen("https://xkcd.com/info.0.json")
     data = response.read()      # a `bytes` object
     text = data.decode('utf-8') # a `str`; this step can't be used if data is binary
     data = json.loads(text)
-    return data['img']   
+    return data['img']
+
