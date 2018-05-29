1ff105a9c4e73a5e3084b26530cdaae93869542d
Roman Peters <romanpeters1993@gmail.com>
2018-05-29 13:27:45 +0200

    Added params

--- devbot/commands/xkcd.py
+++ devbot/commands/xkcd.py
@@ -6,7 +6,7 @@ from devbot.registry import Command
 from devbot.tools import api_requests
 
 @Command(["xkcd"])
-async def xkcd():
+async def xkcd(*_args, **_kwargs):
     url = "https://xkcd.com/info.0.json"
     image_url = api_requests.get_text(url, ("data", "img"))
     return image_url
