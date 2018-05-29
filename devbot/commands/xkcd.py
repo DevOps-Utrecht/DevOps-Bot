ee25e2f3bc9d6f5dbb023bc4b8e1fbd43ba92acc
Roman Peters <romanpeters1993@gmail.com>
2018-05-29 13:18:46 +0200

    Replaced urllib with devbot.api_requests

--- devbot/commands/xkcd.py
+++ devbot/commands/xkcd.py
@@ -3,14 +3,10 @@ import json
 import urllib.request
 
 from devbot.registry import Command
-
+from devbot.tools import api_requests
 
 @Command(["xkcd"])
 async def xkcd():
     url = "https://xkcd.com/info.0.json"
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        data = response.read()   # a `bytes` object
-    text = data.decode('utf-8')  # a `str`; this step can't be used if data is binary
-    data = json.loads(text)
-    return data['img']
+    image_url = api_requests.get_text(url, ("data", "img"))
+    return image_url
