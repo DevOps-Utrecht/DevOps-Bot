1851b6cf5e272b321970810c18c94c79e4e2b8a4
Roman Peters <romanpeters1993@gmail.com>
2018-05-29 13:59:02 +0200

    Type hint

--- devbot/commands/xkcd.py
+++ devbot/commands/xkcd.py
@@ -3,10 +3,10 @@ import json
 import urllib.request
 
 from devbot.registry import Command
-from devbot.tools import api_requests
+from devbot.tools import api_requests as API
 
 @Command(["xkcd"])
-async def xkcd(*_args, **_kwargs):
+async def xkcd(*_args, **_kwargs) -> str:
     url = "https://xkcd.com/info.0.json"
-    image_url = api_requests.get_text(url, ("data", "img"))
+    image_url = API.get_text(url, ("data", "img"))
     return image_url
