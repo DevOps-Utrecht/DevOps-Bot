9d16ac40881491d5d2ae1be492ceb68fba948060
Roman Peters <mail@romanpeters.nl>
2018-11-22 21:42:45 +0100

    Doodle not found message

--- devbot/commands/doodle.py
+++ devbot/commands/doodle.py
@@ -1,13 +1,13 @@
 """ Doodle link """
 import devbot.database as db
-from devbot.registry import Command
+from devbot.registry import Command, Keyword
 
 
 @Keyword(["https://doodle.com/poll/"])
 async def doodle_url(message_contents, *_args, **_kwargs):
     """ Called when a message contains a Doodle poll URL and saves it to the database """
     doodle_url = [i for i in message_contents if "https://doodle.com/poll/" in i][0]
-    
+
     session = db.Session()
     session.merge(db.StringStates(key="doodle_url", value=doodle_url))
     session.commit()
@@ -21,4 +21,7 @@ async def doodle(*_args, **_kwargs):
     session = db.Session()
     entry = session.query(db.StringStates).filter_by(key="doodle_url").first()
     session.close()
-    return entry.value
+    if entry:
+        return entry.value
+    else:
+        return "No Doodle found"
